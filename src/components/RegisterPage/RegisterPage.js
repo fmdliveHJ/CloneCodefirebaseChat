bde371c8e79c701799644c89d4f788b57088d717
hyeonjin <gdalove@naver.com>
2023-01-31 23:58:21 +0900

    form 화면 설정

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  return (
+    <div className="register">
+      <div style={{ textAlign: "center" }}>
+        <h3>Register</h3>
+      </div>
+      <form>
+        <label>email</label>
+        <input defaultValue="test" name="email" type="email" />
+        <label>name</label>
+        <input name="name" type="text" />
+        <label>password</label>
+        <input name="password" type="password" />
+        <label>password confirm</label>
+        <input name="password_confirm" type="pasword" />
+        {/* {...register("exampleRequired", { required: true })}  */}
+
+        {/* {errors.exampleRequired && <p>This field is required</p>} */}
+
+        <input type="submit" />
+      </form>
+      <Link to="/login" style={{ color: "gray", textDecoration: "none" }}>
+        이미 아이디가 있다면...
+      </Link>
+    </div>
+  );
 };
 
 export default RegisterPage;

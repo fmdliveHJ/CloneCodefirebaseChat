5999a14656c5c961467eb6b7401f0693a2588d9d
hyeonjin <gdalove@naver.com>
2023-02-02 22:31:03 +0900

    리액트 훅 폼

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -1,7 +1,20 @@
-import React from "react";
+import { sendPasswordResetEmail } from "firebase/auth";
+import React, { useRef } from "react";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 const RegisterPage = () => {
+  const {
+    register,
+    watch,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const password = useRef();
+  //watch를 이용하여 password값 가져옴
+  password.current = watch("password");
+  console.log(password.current);
   return (
     <div className="register">
       <div style={{ textAlign: "center" }}>
@@ -9,16 +22,43 @@ const RegisterPage = () => {
       </div>
       <form>
         <label>email</label>
-        <input defaultValue="test" name="email" type="email" />
+        <input
+          defaultValue="test"
+          name="email"
+          type="email"
+          {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
+        />
+        {errors.email && <p>This field is required</p>}
         <label>name</label>
-        <input name="name" type="text" />
-        <label>password</label>
-        <input name="password" type="password" />
-        <label>password confirm</label>
-        <input name="password_confirm" type="pasword" />
-        {/* {...register("exampleRequired", { required: true })}  */}
+        <input
+          name="name"
+          type="text"
+          {...register("name", { required: true, maxLength: 10 })}
+        />
+        {errors.name && errors.name.type === "required" && (
+          <p>This field is required</p>
+        )}
+        {errors.name && errors.name.type === "maxLength" && (
+          <p>This field is required</p>
+        )}
+        <label>Password</label>
 
-        {/* {errors.exampleRequired && <p>This field is required</p>} */}
+        <input
+          name="password"
+          type="password"
+          {...register("password", { required: true, minLength: 6 })}
+        />
+
+        <label>Password Confirm</label>
+
+        <input
+          name="password_confirm"
+          type="password"
+          {...register("password_confirm", {
+            required: true,
+            validate: (value) => value === password.current,
+          })}
+        />
 
         <input type="submit" />
       </form>
